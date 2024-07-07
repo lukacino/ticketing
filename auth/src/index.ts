0559c73669755cb39bd6903c50e43cc375eb69be
lukacino <lukacino33@gmail.com>
2024-07-07 11:25:47 +0200

    First commit for ticketing(microservice-course)

---
+++ auth/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import { json } from "body-parser";
+
+import { currentUserRouter } from "./routes/currentUser";
+import { signInRouter } from "./routes/signIn";
+import { signUpRouter } from "./routes/signUp";
+import { signOutRouter } from "./routes/signOut";
+const app = express();
+
+app.use(json());
+
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000");
+});
